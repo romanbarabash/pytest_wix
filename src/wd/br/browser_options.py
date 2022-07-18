e6e56d22922d086d9cc5cb85595883fe633048f2
sema4-romanbarabash <L!verpool1970>
2022-07-18 14:45:04 +0300

    refactoring

--- src/wd/br/browser_options.py
+++ src/wd/br/browser_options.py
@@ -11,6 +11,7 @@ def get_options():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument('--disable-dev-shm-usage')
 
     # options.add_argument("--start-maximized")
     # options.add_argument('--ignore-ssl-errors=yes')
