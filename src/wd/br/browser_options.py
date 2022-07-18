f91fcab5c0c9b7345ec877dfb7fd5d1322c1124b
sema4-romanbarabash <L!verpool1970>
2022-07-18 14:25:34 +0300

    refactoring

--- src/wd/br/browser_options.py
+++ src/wd/br/browser_options.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-from config import DEBUG
 from src.wd.br.browser_manger import browser_manager
 
 DEFAULT_BROWSER_NAME = 'default'
@@ -9,18 +8,21 @@ DEFAULT_BROWSER_NAME = 'default'
 
 def get_options():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
 
-    options.add_argument("--start-maximized")
-    options.add_argument('--ignore-ssl-errors=yes')
-    options.add_argument('--ignore-certificate-errors')
-
-    if not DEBUG:
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--window-size=1920,1080')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
-
+    # options.add_argument("--start-maximized")
+    # options.add_argument('--ignore-ssl-errors=yes')
+    # options.add_argument('--ignore-certificate-errors')
+    #
+    # if not DEBUG:
+    #     options.add_argument('--headless')
+    #     options.add_argument('--no-sandbox')
+    #     options.add_argument('--window-size=1920,1080')
+    #     options.add_argument('--disable-dev-shm-usage')
+    #     options.add_argument('--disable-gpu')
+    #
     options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
 
     return options
