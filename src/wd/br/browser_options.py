5fe86222208323d9bda116a308803e5f0c7ed740
sema4-romanbarabash <L!verpool1970>
2022-07-18 12:23:37 +0300

    change default timeout

--- src/wd/br/browser_options.py
+++ src/wd/br/browser_options.py
@@ -9,16 +9,11 @@ DEFAULT_BROWSER_NAME = 'default'
 
 def get_options():
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument('--ignore-ssl-errors=yes')
-    options.add_argument('--ignore-certificate-errors')
-
-    if not DEBUG:
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--window-size=1920,1080')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugin-port=9222")
+    options.add_argument("--screen-size=1200x800")
 
     # profile = {'plugins.always_open_pdf_externally': True,
     #            'download.default_directory': get_path(DOWNLOADS_DIR),
@@ -30,9 +25,9 @@ def get_options():
     return options
 
 
-
 def open_browser(browser_name: str = DEFAULT_BROWSER_NAME):
-    init_webdriver = webdriver.Chrome(ChromeDriverManager().install(), options=get_options())
+    init_webdriver = webdriver.Chrome(ChromeDriverManager().install(),
+                                      desired_capabilities=get_options().to_capabilities())
     browser_manager.open_browser(init_webdriver, browser_name)
     browser_manager.driver.implicitly_wait(0)  # should be zero
 
