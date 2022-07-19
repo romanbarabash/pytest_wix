364b1ca6c3f1c05b91c24c2eda6cf3cfc9a04c27
sema4-romanbarabash <L!verpool1970>
2022-07-19 11:18:15 +0300

    refactoring

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ env.read_env(verbose=True)
 TIMEOUT = env.int('TIMEOUT', 10)
 POLLING = env.float('POLLING', 0.5)
 
+DEBUG = env.bool('DEBUG', False)
+
 HOST = env.str('HOST')
 USER_EMAIL = env.str('USER_EMAIL')
 USER_PASSWORD = env.str('USER_PASSWORD')
--- src/wd/br/browser_options.py
+++ src/wd/br/browser_options.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+from config import DEBUG
 from src.wd.br.browser_manger import browser_manager
 
 DEFAULT_BROWSER_NAME = 'default'
@@ -8,11 +9,15 @@ DEFAULT_BROWSER_NAME = 'default'
 
 def get_options():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    # options.add_argument("--window-size=1920,1080")
-    options.add_argument("--start-maximized")
-    options.add_argument('--disable-dev-shm-usage')
+    if DEBUG:
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument('--window-size=1920,1080')
+        options.add_argument('--disable-dev-shm-usage')
+    else:
+        options.add_argument("--start-maximized")
+        options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+
     return options
 
 
